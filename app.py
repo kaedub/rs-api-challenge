ac1a30c74956c6b30845c99475a279d065e0098a
Kevin Welch <kaedub16@gmail.com>
2019-01-13 02:58:57 -0800

    Started function factory pattern

--- app.py
+++ app.py
@@ -5,16 +5,24 @@ from models import User, Location, search_users
 
 app = Flask(__name__)
 
-@app.route('/users')
-@validate_query_params
-def users():
-    results = search_users(request.args)
 
-    return jsonify({
-        "metadata": {
-            "path": request.path,
-            # "query": queries
-        },
-        "num_results": len(results),
-        "results": results
-    })
+
+def create_app(test_config=None):
+    """create and configure the app"""
+    app = Flask(__name__, instance_relative_config=True)
+
+    @app.route('/users')
+    @validate_query_params
+    def users():
+        results = search_users(request.args)
+
+        return jsonify({
+            "metadata": {
+                "path": request.path,
+                # "query": queries
+            },
+            "num_results": len(results),
+            "results": results
+        })
+    
+    return app
