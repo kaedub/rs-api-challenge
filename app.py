6f52f2d86b46a03c7ae0e7b87a0bf24eddea7b45
Kevin Welch <kaedub16@gmail.com>
2019-01-13 02:00:21 -0800

    Used a decorator pattern to add a query parameter validation feature to the route

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
 from flask import Flask, request, jsonify
+from flask_json_schema import JsonSchema
+from validation import validate_query_params
 from models import User, Location, search_users
 
 app = Flask(__name__)
 
-def validate_queries(queries):
-    gender = queries.get('gender')
-    dist = float(queries.get('dist',0))
-    origin = queries.get('origin')
-    origin = list(map(lambda e: float(e), origin.split(','))) if origin else None
-    min_age = int(queries.get('min_age',0))
-    max_age = int(queries.get('max_age',0)) or None
-    return {
-        "gender": gender,
-        "dist": dist,
-        "origin": origin,
-        "min_age": min_age,
-        "max_age": max_age
-    }
-
 @app.route('/users')
+@validate_query_params
 def users():
-    queries = validate_queries(dict(request.args.items()))
-
-    results = search_users(queries)
+    results = search_users(request.args)
 
     return jsonify({
         "metadata": {
             "path": request.path,
-            "query": queries
+            # "query": queries
         },
         "num_results": len(results),
         "results": results
---
+++ validation.py
@@ -0,0 +1,25 @@
+from flask import request
+from functools import wraps
+
+def _validate_queries(queries):
+    gender = queries.get('gender')
+    dist = float(queries.get('dist',0))
+    origin = queries.get('origin')
+    origin = list(map(lambda e: float(e), origin.split(','))) if origin else None
+    min_age = int(queries.get('min_age',0))
+    max_age = int(queries.get('max_age',0)) or None
+    return {
+        "gender": gender,
+        "dist": dist,
+        "origin": origin,
+        "min_age": min_age,
+        "max_age": max_age
+    }
+
+def validate_query_params(func):
+    @wraps(func)
+    def validate(*args, **kwargs):
+        queries = _validate_queries(dict(request.args.items()))
+        request.args = queries
+        return func(*args, **kwargs)
+    return validate
